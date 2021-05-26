527984445b36d694dd053d507372e7f1a4acb734
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-05-26 17:59:46 +0900

    Delete TeacherInfo.py

--- TeacherInfo.py
+++
@@ -1,8 +0,0 @@
-class Teacher:
-    def __init__(self, name, grade, subject):
-        self.name = name
-        self.grade = grade
-        self.subject = subject
-
-    def __str__(self):
-        return self.name+' ' + self.grade+' ' + self.subject
